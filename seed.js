c4aaa9fdc09991659a545af24797f30d6e02e4de
karthik <karthikchinu777@gmail.com>
2020-07-25 17:07:01 +0530

    created Upvote app

---
+++ seed.js
@@ -0,0 +1,29 @@
+const submissions = [
+  {
+    id: 1,
+    title: "Yellow Pillo",
+    description: " On-demand sand castle construction expertise.",
+    url: "#",
+    votes: 8,
+    avatar: "../images/download.png",
+    submissionImage: "../images/download.png",
+  },
+  {
+    id: 2,
+    title: "green Pillo",
+    description: " On-demand sand castle construction expertise.",
+    url: "#",
+    votes: 10,
+    avatar: "../images/download.png",
+    submissionImage: "../images/download.png",
+  },
+  {
+    id: 3,
+    title: "purple Pillo",
+    description: " On-demand sand castle construction expertise.",
+    url: "#",
+    votes: 11,
+    avatar: "../images/download.png",
+    submissionImage: "../images/download.png",
+  },
+];
